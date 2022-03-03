ceee3fb82a2b8056c79a036586054e8b620c62c3
xiaoyuej <xiaoyuej@welldeliver.cn>
2022-03-03 17:30:44 +0800

    登录验证码

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -10,6 +10,7 @@
                     label="用户名"
                     placeholder="用户名"
                     :rules="[{ required: true, message: '请填写用户名' }]"
+                    center
                 />
                 <van-field
                     v-model="form.password"
@@ -18,18 +19,20 @@
                     label="密码"
                     placeholder="密码"
                     :rules="[{ required: true, message: '请填写密码' }]"
+                    center
                 />
                 <van-field
-                    v-model="form.captcha"
-                    type="captcha"
+                    v-model="form.verify"
+                    type="verify"
                     name="验证码"
                     label="验证码"
                     placeholder="验证码"
                     :rules="[{ required: true, message: '请输入验证码' }]"
+                    center
                 >
-                    <div>
-                        <img src alt />
-                    </div>
+                    <template #button>
+                        <img-verify ref="verifyRef"></img-verify>
+                    </template>
                 </van-field>
                 <div class="register">
                     <span @click="register">立即注册</span>
@@ -43,42 +46,64 @@
     </div>
 </template>
 
-<script setup>
-import { reactive } from 'vue'
+<script >
+import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router';
 import { login } from '../../api/login'
 import { Toast } from 'vant';
 import md5 from 'js-md5'
-const router = useRouter()
-const form = reactive({
-    username: '13281331383',
-    password: '123456',
-    verify: ''
-})
-const onSubmit = () => {
-    let data = {
-        loginName:form.username,
-        passwordMd5:md5(form.password)
+import imgVerify from '../../components/imgVerify.vue';
+import ImgVerify from '../../components/imgVerify.vue';
+
+export default {
+    components: {
+        imgVerify,
+        ImgVerify
+    },
+    setup() {
+        const router = useRouter()
+        const verifyRef = ref(null)
+        const form = reactive({
+            username: '13281331383',
+            password: '123456',
+            verify: ''
+        })
+        const onSubmit = () => {
+            if (form.verify.toUpperCase() == verifyRef.value.state.imgCode) {
+                let data = {
+                    loginName: form.username,
+                    passwordMd5: md5(form.password)
+                }
+                login(data).then((res) => {
+                    if (res.data.resultCode == 200) {
+                        Toast.success('登录成功');
+                        router.push('/index')
+                        localStorage.setItem('token', res.data.data)
+                    }
+                    else {
+                        Toast.fail('账号密码错误');
+                    }
+                })
+            }else{
+                Toast.fail('验证码错误');
+            }
+
+        }
+        const register = () => {
+            router.push('/register')
+        }
+        const onClickLeft = () => { history.back() }
+        return {
+            form,
+            onSubmit,
+            register,
+            onClickLeft,
+            verifyRef
+        }
     }
-    login(data).then((res) => {
-        if (res.data.resultCode == 200) {
-            Toast.success('登录成功');
-            router.push('/index')
-            localStorage.setItem('token',res.data.data)
-        }
-        else {
-            Toast.fail('账号密码错误');
-        }
-    })
-}
-const register = () => {
-    router.push('/register')
 }
 
-const onClickLeft = () =>{
-    console.log(222)
-    router.go(-1)
-}
+
 </script>
 
 <style lang="less" scoped>
